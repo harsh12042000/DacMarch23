962918164c3f3806d7e4cb86d8dcc6dbb88b713d
Harshada Sonawane <49424723+harsh12042000@users.noreply.github.com>
2023-03-31 08:05:33 +0530

    Add files via upload

--- OOPJ/Assignment_1/Assignment1_28.java
+++ OOPJ/Assignment_1/Assignment1_28.java
@@ -5,12 +5,14 @@ b. double value into Double instance.
 c. String instance into Double instance.
 d. double value into binary, octal and hexadecimal 
 string(Note: Here you can use doubleToLongBits() method 
-along with methods of Long class).
+along with methods of Long class.
  */
 public class Assignment1_28 {
     public static void main(String[] args) {
         
         double s = 1001.1521;
+        long l = Double.doubleToLongBits(s);
+
         String str = Double.toString(s);
         System.out.println("Double to String: " + str);
 
@@ -20,6 +22,10 @@ public class Assignment1_28 {
         String str2 = "20022.145";
         Double sObj2 = new Double(Double.valueOf(str2));
         System.out.println("String to Double instance: " + sObj2);
+
+        System.out.println("long value into binary : " + Long.toBinaryString(l));
+        System.out.println("long value into octal : " + Long.toOctalString(l));
+        System.out.println("long value into hexadecimal : " + Long.toHexString(l));
 
     }
 }
